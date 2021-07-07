26fdc733a779bd50066ebcfb3445c83d71605a66
Eduardo Pacheco <eduardo.pach@hotmail.com>
2021-07-06 23:59:31 -0300

    cstr.jl
    Corrected cstr.jl code and added plots

--- src/cstr.jl
+++ src/cstr.jl
@@ -4,30 +4,27 @@ using Plots,DifferentialEquations
 function cstr(du,u,p,t)
 
     Ca,T = u # States
-    dCa,dT = du # Derivatives
     Ea,k₀,V,ρ,Cₚ,ΔH,UA,q,Caf,Tf,Tc = p # Parameters
+    k(x) = k₀*exp(-Ea/8.314/x) # kinetic constant
 
-
-    k(T) = k₀*exp(-Ea/8.314/T) # kinectic constant
-
-    dCa = (q/V)*(Caf-Ca) - k(T)*Ca # derivatie of Ca
-    dT = (q/V)*(Tf-T) + (-ΔH/ρ/Cₚ)*k(T)*Ca + (UA/V/ρ/Cₚ)*(Tc-T) # Derivative of T
+    du[1] = dCa = (q/V)*(Caf-Ca) - k(T)*Ca # derivatie of Ca
+    du[2]  = dT = (q/V)*(Tf-T) + (-ΔH/ρ/Cₚ)*k(T)*Ca + (UA/V/ρ/Cₚ)*(Tc-T) # Derivative of T
 
 end
 ####################################################################
 
 ########################### PARAMETERS #############################
-Ea = 72750     
-k₀ = 7.2e10    
-V = 100.0     
-ρ = 1000.0    
-Cₚ = 0.239     
-ΔH = -5.0e4 
-UA = 5.0e4 
-q = 100.0
-Caf = 1.0
-Tf = 350.0
-Tc = 300.0
+Ea = 72750          # activation energy J/gmol
+k₀ = 7.2e10         # Arrhenius rate constant 1/min
+V = 100.0           # Volume [L]
+ρ = 1000.0          # Density [g/L]
+Cₚ = 0.239          # Heat capacity [J/g/K]
+ΔH = -5.0e4         # Enthalpy of reaction [J/mol]
+UA = 5.0e4          # Heat transfer [J/min/K]
+q = 100.0           # Flowrate [L/min]
+Caf = 1.0           # Inlet feed concentration [mol/L]
+Tf = 350.0          # Inlet feed temperature [K]
+Tc = 300.0          # Coolant temperature [K]
 ####################################################################
 
 ########################### INITIAL COND ###########################
@@ -40,9 +37,9 @@ u0 = [Ca0 , T0]
 p = [Ea,k₀,V,ρ,Cₚ,ΔH,UA,q,Caf,Tf,Tc]
 tspan = (0.0 , 10.0)
 prob = ODEProblem(cstr,u0,tspan,p)
-sol = solve(prob)
+sol = solve(prob,Tsit5(),saveat=0.1)
 ####################################################################
 
 ########################### PLOTTING SOLUTION #############################
-plot(sol,layout=(2,1))
-
+plot(sol,layout=(1,2))
+plot(sol,vars=(1,2),ylabel="T",xlabel="Ca",title="Phase Space")
