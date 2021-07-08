02a483591470f71f6a8759c0aa3d8b465e0e4d46
Eduardo Pacheco <eduardo.pach@hotmail.com>
2021-07-08 17:19:08 -0300

    Code Update
    Added an animation in the cstr.jl, however I couldn't figure it out how to add a suptitlte (like in matplotlib) in order to show the current coolant temperature in the animation. I've also added Measures to the project to change the left_margin in the subplots for the animation.

--- src/cstr.jl
+++ src/cstr.jl
@@ -1,6 +1,7 @@
-using Plots,DifferentialEquations
+using Plots: length, label_to_string
+using Plots,DifferentialEquations,Measures
 
-########################### CSTR MODEL #############################
+############################################### CSTR FUNCTION #################################################################
 function cstr(du,u,p,t)
 
     Ca,T = u # States
@@ -11,9 +12,9 @@ function cstr(du,u,p,t)
     du[2]  = dT = (q/V)*(Tf-T) + (-ΔH/ρ/Cₚ)*k(T)*Ca + (UA/V/ρ/Cₚ)*(Tc-T) # Derivative of T
 
 end
-####################################################################
+##############################################################################################################################
 
-########################### PARAMETERS #############################
+############################################### PARAMETERS ###################################################################
 Ea = 72750          # activation energy J/gmol
 k₀ = 7.2e10         # Arrhenius rate constant 1/min
 V = 100.0           # Volume [L]
@@ -24,36 +25,68 @@ UA = 5.0e4          # Heat transfer [J/min/K]
 q = 100.0           # Flowrate [L/min]
 Caf = 1.0           # Inlet feed concentration [mol/L]
 Tf = 350.0          # Inlet feed temperature [K]
-Tc = 300.0          # Coolant temperature [K]
-####################################################################
+Tc = 280.0          # Coolant temperature [K]
+##############################################################################################################################
 
-########################### INITIAL COND ###########################
+############################################### INITIAL COND #################################################################
 Ca0 = 0.5
 T0 = 350.0
-####################################################################
+##############################################################################################################################
 
-########################### SIMULATION #############################
-u0 = [Ca0 , T0]
-p = [Ea,k₀,V,ρ,Cₚ,ΔH,UA,q,Caf,Tf,Tc]
-tspan = (0.0 , 10.0)
-prob = ODEProblem(cstr,u0,tspan,p)
-sol = solve(prob,Tsit5(),saveat=0.1)
-####################################################################
+############################################### SIMULATION ###################################################################
+u0 = [Ca0 , T0] # Initial Condition Array
+p = [Ea,k₀,V,ρ,Cₚ,ΔH,UA,q,Caf,Tf,Tc] # Parameters Array
+tspan = (0.0 , 10.0) # Creating time span to solve the ODE from 0 to 10 min
+prob = ODEProblem(cstr,u0,tspan,p) # Creating the ODEProblem
+sol = solve(prob,Tsit5(),saveat=0.1) # Solving the problem and saving the results at each 0.1 min
+##############################################################################################################################
 
-########################### PLOTTING SOLUTION #############################
-gr()
-t = 0:0.1:10
-T_react = sol[2,:]
-Ca_react = sol[1,:]
-p_Ca = plot(t,Ca_react,title="Reactor's Concentration over Time",ylabel="Ca (mol/L)",
-            color=:red,label="");
+############################################### PLOTTING SOLUTION ###############################################################
+gr(size=(1000,1000)) # Backend choice
+t = 0:0.1:10 # Time array
+T_react = sol[2,:] # Getting Temperature solution vector
+Ca_react = sol[1,:] # Getting Concentration solution vector
+p_Ca = plot(t,Ca_react,title="Reactor's Concentration",ylabel="Ca (mol/L)",
+            color=:red,label=""); # Creating a plot to Concentration
             
-p_T = plot(t,T_react,title="Reactor's Temperature over Time",ylabel="Temperature (K)",
-            xlabel="Time (min)",color=:orange,label="");
+p_T = plot(t,T_react,title="Reactor's Temperature",ylabel="Temperature (K)",
+            xlabel="Time (min)",color=:orange,label=""); # Creating a plot to Temperature
 
-transient_plot = plot(p_Ca,p_T,layout=(2,1))
-savefig(transient_plot,"Images/transient_plot.png")
+transient_plot = plot(p_Ca,p_T,layout=(2,1)) # Adding both plots to a grid layout of 2 rows and 1 column
+savefig(transient_plot,"Images/transient_plot.png") # Saving the transient_plot
 
 phase_plot = plot(sol,vars=(1,2),ylabel="Temperature (K)",xlabel="Concentration (mol/L)",
-                    title="Phase Space",label="")
-savefig(phase_plot,"Images/phase_plot.png")
+                    title="Phase Space",label="") # Creating a plot from the States (Ca,T) a.k.a Phase Plot
+savefig(phase_plot,"Images/phase_plot.png") # Saving the phase_plot
+##############################################################################################################################
+
+############################################### ANIMATION PLOT ###############################################################
+
+plot(p_Ca,p_T,phase_plot,layout=@layout([[a;b] c]),left_margin=5mm,line_z=1:length(t))
+
+# This function simulates the CSTR for a giving Coolant Temerature.
+function sim_cstr(Tc)
+    u0 = [Ca0 , T0] # Initial Condition Array
+    p = [Ea,k₀,V,ρ,Cₚ,ΔH,UA,q,Caf,Tf,Tc] # Parameters Array
+    tspan = (0.0 , 10.0) # Creating time span to solve the ODE from 0 to 10 min
+    prob = ODEProblem(cstr,u0,tspan,p) # Creating the ODEProblem
+    sol = solve(prob,Tsit5(),saveat=0.1) # Solving the problem and saving the results at each 0.1 min
+    return sol
+end
+
+Tcs = 280:0.5:350 # Array of Coolant Temperatures 
+sols = sim_cstr.(Tcs); # Solving for each Coolant Temperature in Tcs
+
+# Creating animation using the animate macro
+anim = @animate for i ∈ 1:length(Tcs)
+    # Plots each solution in the sols array (these makes a new plot for each solution, thus older solutions don't appear)
+    plot(
+        plot(sols[i].t,sols[i][1,:],title="Reactor's Concentration",ylabel="Ca (mol/L)",label=:none),
+        plot(sols[i].t,sols[i][2,:],title="Reactor's Temperature",ylabel="Temperature (K)",xlabel="Time (min)",label=:none),
+        plot(sols[i],vars=(1,2),ylabel="Temperature (K)",xlabel="Concentration (mol/L)",title="Phase Space",label=:none),
+        layout=@layout([[a;b] c]),suptitle="Coolant Temperature = ",left_margin=5mm
+    )
+    scatter!((Ca0,T0),sp=3,label="Initial Condition",markercolor=:black) # Adds a point in the phase plot regarding the initial condition
+end
+gif(anim, "Images/cstr_coolant_effect.gif", fps = 10) # create a gif using the anim variable where we've stored our frames
+##############################################################################################################################
